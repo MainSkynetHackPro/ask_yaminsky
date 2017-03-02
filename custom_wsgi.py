fbf9054b0e4d8f0ce103b9c85993ec496e478bfa
Nikolay Yaminsky <yaminsky96@gmail.com>
2017-03-02 17:04:21 +0300

    wsgi + startapp

---
+++ custom_wsgi.py
@@ -0,0 +1,28 @@
+from cgi import urlparse, escape
+
+
+def connection_handler(environ, start_response):
+    get_data = urlparse.parse_qs(environ.get('QUERY_STRING', ''))
+
+    try:
+        request_body_size = int(environ.get('CONTENT_LENGTH', 0))
+    except (ValueError):
+        request_body_size = 0
+
+    request_body = environ['wsgi.input'].read(request_body_size)
+    post_data = urlparse.parse_qs(request_body)
+
+    form = '''
+        <form action="/" method="POSt">
+        <input name="text" type="test">
+        <input type="submit">
+        </form>
+    '''
+    start_response('200 OK', [('Content-Type', 'text/html')])
+    return 'GET: {0}<br> POST: {1}<br>{2}'.format(get_data, post_data, form)
+
+
+if __name__ == '__main__':
+    from wsgiref.simple_server import make_server
+    srv = make_server('localhost', 8080, connection_handler)
+    srv.serve_forever()
