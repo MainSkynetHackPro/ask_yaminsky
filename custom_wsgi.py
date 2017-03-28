b2e61221cacb0a369800add1f33c8257f263103f
Nikolay Yaminsky <yaminsky96@gmail.com>
2017-03-28 19:08:53 +0300

    wsgi

--- custom_wsgi.py
+++ custom_wsgi.py
@@ -1,13 +1,34 @@
+from cgi import urlparse, escape
+
+
 def app(environ, start_response):
+    get_data = urlparse.parse_qs(environ.get('QUERY_STRING', ''))
+
+    try:
+        request_body_size = int(environ.get('CONTENT_LENGTH', 0))
+    except (ValueError):
+        request_body_size = 0
+
+    request_body = environ['wsgi.input'].read(request_body_size)
+    post_data = urlparse.parse_qs(request_body)
+
+    if post_data:
+        print 'POST: {0}'.format(post_data)
+
+    if get_data:
+        print 'GET: {0}'.format(get_data)
+
+    form = '''
+        <form action="/" method="POSt">
+        <input name="text" type="test">
+        <input type="submit">
+        </form>
+    '''
     start_response('200 OK', [('Content-Type', 'text/html')])
-    print()
-    return [b'Hello, Finn the human!']
+    return 'GET: {0}<br> POST: {1}<br>{2}'.format(get_data, post_data, form)
+
 
 if __name__ == '__main__':
-    try:
-        from wsgiref.simple_server import make_server
-        httpd = make_server('', 8081, app)
-        print('Serving on port 8081...')
-        httpd.serve_forever()
-    except KeyboardInterrupt:
-        print('Goodbye.')
+    from wsgiref.simple_server import make_server
+    srv = make_server('localhost', 8080, app)
+    srv.serve_forever()
