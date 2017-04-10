c08b797fc8d4903b26116e6f20645195967b15f4
Nikolay <yaminsky96@gmail.com>
2017-04-10 12:31:24 +0300

    init models

--- ask/models.py
+++ ask/models.py
@@ -1,5 +1,80 @@
+# encoding=utf8
 from __future__ import unicode_literals
-
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin, UserManager
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(verbose_name=u'Username', max_length=255, unique=True, default=None,
+                                error_messages={'unique': 'User with this username exists'})
+    email = models.EmailField(verbose_name=u'Email', unique=True, default=None, null=True, blank=True)
+
+    is_staff = models.BooleanField(verbose_name=u'has admin access', default=False)
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
+
+    object = UserManager()
+
+    class Meta:
+        verbose_name = u'User'
+        verbose_name_plural = u'Users'
+        swappable = 'AUTH_USER_MODEL'
+
+    def __unicode__(self):
+        return self.username
+
+    def get_short_name(self):
+        return self.username
+
+    def get_full_name(self):
+        return self.username
+
+
+class Tag(models.Model):
+    title = models.CharField(verbose_name=u'Tag title', max_length=255)
+
+    class Meta:
+        verbose_name = u'Tag'
+        verbose_name_plural = u'Tags'
+
+
+class Ask(models.Model):
+    question = models.CharField(verbose_name=u'question', max_length=255)
+    text = models.TextField(verbose_name=u'text', )
+    rating = models.IntegerField(verbose_name=u'Rating', default=0)
+
+    author = models.ForeignKey(User, verbose_name=u'Author')
+
+    tags = models.ManyToManyField(Tag, verbose_name=u'Tags', related_name='asks')
+
+    class Meta:
+        verbose_name = u'Ask'
+        verbose_name_plural = u'Asks'
+
+
+class Answer(models.Model):
+    title = models.CharField(verbose_name=u'Title', max_length=255)
+    text = models.TextField(verbose_name=u'Text')
+    rating = models.IntegerField(verbose_name=u'Rating', default=0)
+
+    ask = models.ForeignKey(Ask, verbose_name=u'Ask', related_name='answers')
+    author = models.ForeignKey(User, verbose_name=u'Author')
+
+    class Meta:
+        verbose_name = u'Answer'
+        verbose_name_plural = u'Answers'
+
+
+class UserVote(models.Model):
+    author = models.ForeignKey(User, verbose_name=u'Author')
+    ask = models.ForeignKey(Ask, verbose_name=u'Ask', related_name='votes', null=True, blank=True)
+    answer = models.ForeignKey(Answer, verbose_name=u'Answer', related_name='votes', null=True, blank=True)
+
+    delta = models.IntegerField(verbose_name=u'delta')
+
+    class Meta:
+        verbose_name = u'User vote'
+        verbose_name_plural = u'User votes'
