4dc367c06d72387c994941ca3e6db4960df379e9
Nikolay Yaminsky <yaminsky96@gmail.com>
2017-05-22 23:00:02 +0300

    init

---
+++ static/js/main.js
@@ -0,0 +1,11 @@
+function lazy_warning(text) {
+    new Noty({
+        type: 'warning',
+        text: text,
+        timeout: 1500
+    }).show();
+}
+$(document).ready(function () {
+
+
+});
---
+++ static/js/vote.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+
+    $(".vote-up").on("click", function (e) {
+        e.preventDefault();
+        var ask_id = $(this).data('ask');
+
+
+
+        lazy_warning('tesstsas');
+
+    })
+});
